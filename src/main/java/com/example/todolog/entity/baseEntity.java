c22dc9c1ec1f3a5e5970fa7be1459d19b3e3b07f
searchcolor <qkrdydwo1022@gmail.com>
2024-11-19 16:47:53 +0900

    test

--- src/main/java/com/example/todolog/entity/baseEntity.java
+++
@@ -1,9 +0,0 @@
-package com.example.todolog.entity;
-
-public class baseEntity {
-    public static void main(String[] args) {
-            int a = 1;
-            int b = 1;
-            int c = 1;
-    }
-}
