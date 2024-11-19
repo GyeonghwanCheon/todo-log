4f5c09f33a5552e20e7897a3a4566c9c744b3b3b
searchcolor <qkrdydwo1022@gmail.com>
2024-11-19 14:27:47 +0900

    feat: add package
    패키지 생성

---
+++ src/main/java/com/example/todolog/TodoLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todolog/TodoLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
